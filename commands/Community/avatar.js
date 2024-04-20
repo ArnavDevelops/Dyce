46464f60e165d71941e00e135119c7371992c472
Arnav <91401687+ArnavDevelops@users.noreply.github.com>
2024-04-21 03:37:34 +0530

    Avatar Command Updated!!
    - U can download the attachment.
    - It will recommend to use commands like userinfo & serverinfo.

--- commands/Community/avatar.js
+++ commands/Community/avatar.js
@@ -1,6 +1,9 @@
 const {
   SlashCommandBuilder,
   EmbedBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  ActionRowBuilder,
 } = require("discord.js");
 
 module.exports = {
@@ -20,7 +23,7 @@ module.exports = {
         )
     )
     .addSubcommand((option) =>
-      option.setName("guild").setDescription("Check avatar of the server.")
+      option.setName("server").setDescription("Check avatar of the server.")
     ),
   async execute(interaction, client) {
     const { options, guild } = interaction;
@@ -30,22 +33,46 @@ module.exports = {
       const user = options.getUser("user") || interaction.user;
       const avatar = user.avatarURL({ dynamic: true, size: 256 });
 
+      const button = new ButtonBuilder()
+        .setLabel('Download')
+        .setStyle(ButtonStyle.Link)
+        .setURL(user.avatarURL())
+
+      const button2 = new ButtonBuilder()
+        .setCustomId('tryuserinfo')
+        .setLabel('Try /userinfo too!')
+        .setStyle(ButtonStyle.Secondary)
+        .setDisabled(true)
+      const row = new ActionRowBuilder().addComponents(button, button2)
+
       const embed = new EmbedBuilder()
         .setTitle(`${user.username}'s Avatar`)
         .setImage(avatar)
         .setColor("Random")
-      await interaction.reply({ embeds: [embed] });
+      await interaction.reply({ embeds: [embed], components: [row] });
     }
 
     //Guild
-    if (options.getSubcommand() === "guild") {
+    if (options.getSubcommand() === "server") {
       const avatar = guild.iconURL({ dynamic: true, size: 256 });
+
+      const button = new ButtonBuilder()
+      .setLabel('Download')
+      .setStyle(ButtonStyle.Link)
+      .setURL(guild.iconURL())
+
+    const button2 = new ButtonBuilder()
+      .setCustomId('trrguildinfo')
+      .setLabel('Try /serverinfo too!')
+      .setStyle(ButtonStyle.Secondary)
+      .setDisabled(true)
+    const row = new ActionRowBuilder().addComponents(button, button2)
 
       const embed = new EmbedBuilder()
         .setTitle(`${guild.name}'s Avatar`)
         .setImage(avatar)
         .setColor("Random");
-      await interaction.reply({ embeds: [embed] });
+      await interaction.reply({ embeds: [embed], components: [row] });
     }
   },
 };
