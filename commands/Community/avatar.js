f32f4e07110011525b5923652b0b125edc1d4a34
Arnav <91401687+ArnavDevelops@users.noreply.github.com>
2024-05-28 16:16:58 +0530

    Avatar Update
    Can choose between either checking server avatar or normal discord avatar!

--- commands/Community/avatar.js
+++ commands/Community/avatar.js
@@ -21,6 +21,15 @@ module.exports = {
             .setDescription("Select a user")
             .setRequired(false)
         )
+        .addStringOption((s) =>
+          s
+            .setName("type")
+            .setDescription("Choose the type of avatar")
+            .addChoices(
+              { name: "Default Avatar", value: "default" },
+              { name: "Server Avatar", value: "server" }
+            )
+        )
     )
     .addSubcommand((option) =>
       option.setName("server").setDescription("Check avatar of the server.")
@@ -31,42 +40,95 @@ module.exports = {
     //User
     if (options.getSubcommand() === "user") {
       const user = options.getUser("user") || interaction.user;
-      const avatar = user.avatarURL({ dynamic: true, size: 256 });
+      const type = options.getString("type")
 
-      const button = new ButtonBuilder()
-        .setLabel('Download')
-        .setStyle(ButtonStyle.Link)
-        .setURL(user.avatarURL({ size: 2048 }))
+      if (type == "default" || !type) {
+        const avatar = user.avatarURL({ dynamic: true, size: 4096 });
 
-      const button2 = new ButtonBuilder()
-        .setCustomId('tryuserinfo')
-        .setLabel('Try /userinfo too!')
-        .setStyle(ButtonStyle.Secondary)
-        .setDisabled(true)
-      const row = new ActionRowBuilder().addComponents(button, button2)
+        const button = new ButtonBuilder()
+          .setLabel('Download')
+          .setStyle(ButtonStyle.Link)
+          .setURL(user.avatarURL({ size: 4096 }))
 
-      const embed = new EmbedBuilder()
-        .setTitle(`${user.username}'s Avatar`)
-        .setImage(avatar)
-        .setColor("Random")
-      await interaction.reply({ embeds: [embed], components: [row] });
+        const button2 = new ButtonBuilder()
+          .setCustomId('tryuserinfo')
+          .setLabel('Try /userinfo too!')
+          .setStyle(ButtonStyle.Secondary)
+          .setDisabled(true)
+        const row = new ActionRowBuilder().addComponents(button, button2)
+
+        const embed = new EmbedBuilder()
+          .setTitle(`${user.username}'s Avatar`)
+          .setImage(avatar)
+          .setColor("Random")
+        await interaction.reply({ embeds: [embed], components: [row] });
+      } else if (type == "server") {
+        let res = await fetch(`https://discord.com/api/guilds/${guild.id}/members/${user.id}`, {
+          headers: {
+            Authorization: `Bot ${client.token}`
+          },
+        })
+        let data = await res.json()
+
+        if (data.avatar !== undefined && data.avatar !== null) {
+          let avatar = `https://cdn.discordapp.com/guilds/${guild.id}/users/${user.id}/avatars/${data.avatar}.webp?size=4096`
+
+          const button = new ButtonBuilder()
+            .setLabel('Download')
+            .setStyle(ButtonStyle.Link)
+            .setURL(avatar)
+
+          const button2 = new ButtonBuilder()
+            .setCustomId('tryuserinfo')
+            .setLabel('Try /userinfo too!')
+            .setStyle(ButtonStyle.Secondary)
+            .setDisabled(true)
+          const row = new ActionRowBuilder().addComponents(button, button2)
+
+          const embed = new EmbedBuilder()
+            .setTitle(`${user.username}'s Avatar`)
+            .setImage(avatar)
+            .setColor("Random")
+          await interaction.reply({ embeds: [embed], components: [row] });
+        } else {
+          const avatar = user.avatarURL({ dynamic: true, size: 4096 });
+
+          const button = new ButtonBuilder()
+            .setLabel('Download')
+            .setStyle(ButtonStyle.Link)
+            .setURL(user.avatarURL({ size: 4096 }))
+
+          const button2 = new ButtonBuilder()
+            .setCustomId('tryuserinfo')
+            .setLabel('Try /userinfo too!')
+            .setStyle(ButtonStyle.Secondary)
+            .setDisabled(true)
+          const row = new ActionRowBuilder().addComponents(button, button2)
+
+          const embed = new EmbedBuilder()
+            .setTitle(`${user.username}'s Avatar`)
+            .setImage(avatar)
+            .setColor("Random")
+          await interaction.reply({ embeds: [embed], components: [row] });
+        }
+      }
     }
 
     //Guild
     if (options.getSubcommand() === "server") {
-      const avatar = guild.iconURL({ dynamic: true, size: 256 });
+      const avatar = guild.iconURL({ dynamic: true, size: 4096 });
 
       const button = new ButtonBuilder()
-      .setLabel('Download')
-      .setStyle(ButtonStyle.Link)
-      .setURL(guild.iconURL())
+        .setLabel('Download')
+        .setStyle(ButtonStyle.Link)
+        .setURL(guild.iconURL())
 
-    const button2 = new ButtonBuilder()
-      .setCustomId('trrguildinfo')
-      .setLabel('Try /serverinfo too!')
-      .setStyle(ButtonStyle.Secondary)
-      .setDisabled(true)
-    const row = new ActionRowBuilder().addComponents(button, button2)
+      const button2 = new ButtonBuilder()
+        .setCustomId('trrguildinfo')
+        .setLabel('Try /serverinfo too!')
+        .setStyle(ButtonStyle.Secondary)
+        .setDisabled(true)
+      const row = new ActionRowBuilder().addComponents(button, button2)
 
       const embed = new EmbedBuilder()
         .setTitle(`${guild.name}'s Avatar`)
